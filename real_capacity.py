e0b9526aeaaa967854695a689479cd6d852daff5
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-29 10:01:28 -0600

    Update real_capacity.py

--- real_capacity.py
+++ real_capacity.py
@@ -1,1 +1,4 @@
+# Precomputed conversion factors (advertised / real)
+TB_TO_TIB = 1000000000000 / 1099511627776  # 1 TB ≈ 0.9095 TiB
+GB_TO_GIB = 1000000000 / 1073741824        # 1 GB ≈ 0.9313 GiB
 
